6497ec59d603d248372f554879f30c7fd0f3ae5d
phdargen <p.dargent@cern.ch>
2018-10-16 16:59:56 +0200

    PIDCorr scripts for Run2

--- PIDCalib/runPID.sh
+++ PIDCalib/runPID.sh
@@ -1,5 +1,5 @@
 lb-run Urania/v7r0 python ./signal_Run1.py
-#lb-run Urania/v7r0 python ./signal_Run2.py
+lb-run -c x86_64-slc6-gcc7-opt --nightly lhcb-head Urania/HEAD python ./signal_Run2.py
 
 lb-run Urania/v7r0 python ./norm_Run1.py
 #lb-run Urania/v7r0 python ./norm_Run2.py
