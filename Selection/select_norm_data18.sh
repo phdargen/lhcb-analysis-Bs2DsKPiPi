c2b2c427fb3f3f1de7e4899cfd197a30a11ab95d
phdargen <p.dargent@cern.ch>
2020-03-28 12:29:36 +0100

    test 18 data

---
+++ Selection/select_norm_data18.sh
@@ -0,0 +1,8 @@
+./MiniMaker "Norm" "Ds2KKpi" "Data" 18
+./SelectionMaker "Norm" "Ds2KKpi" "Data" 18
+
+./MiniMaker "Norm" "Ds2Kpipi" "Data" 18
+./SelectionMaker "Norm" "Ds2Kpipi" "Data" 18
+
+./MiniMaker "Norm" "Ds2pipipi" "Data" 18
+./SelectionMaker "Norm" "Ds2pipipi" "Data" 18
---
+++ Selection/select_signal_data18.sh
@@ -0,0 +1,8 @@
+./MiniMaker "Signal" "Ds2KKpi" "Data" 18
+./SelectionMaker "Signal" "Ds2KKpi" "Data" 18
+
+./MiniMaker "Signal" "Ds2Kpipi" "Data" 18
+./SelectionMaker "Signal" "Ds2Kpipi" "Data" 18
+
+./MiniMaker "Signal" "Ds2pipipi" "Data" 18
+./SelectionMaker "Signal" "Ds2pipipi" "Data" 18
